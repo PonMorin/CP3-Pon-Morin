32aab95533e6abb0d544127f3aa04f0af11781b2
PonMorin <66424414+PonMorin@users.noreply.github.com>
2020-06-04 20:34:20 +0700

    Create Exercise4_Pon_M.py

---
+++ Exercise4_Pon_M.py
@@ -0,0 +1,11 @@
+F = 90
+G = 75.5
+I = 60
+C = 83.4
+print("------Your Score------")
+print("Foundation English: ",F)
+print("General Business: ",G)
+print("Introduction to Computer Systems: ",I)
+print("Computer Programming: ",C)
+C = 83.4
+print("----------------------")
