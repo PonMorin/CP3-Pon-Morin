29866beff86610f9b2ca27cd1fb93d0aaf58693e
PonMorin <66424414+PonMorin@users.noreply.github.com>
2020-06-06 11:30:59 +0700

    Create Exercise5_1_Pon_M.py

---
+++ Exercise5_1_Pon_M.py
@@ -0,0 +1,12 @@
+num1 = int(input("กรุณาใส่ตัวเลข: "))
+num2 = int(input("กรุณาใส่ตัวเลข: "))
+
+add  = num1 + num2
+sub  = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+
+print("คำตอบผลบวก:",add)
+print("คำตอบผลลบ:",sub)
+print("คำตอบผลคูณ:",multiply)
+print("คำตอบผลหาร:",divide)
