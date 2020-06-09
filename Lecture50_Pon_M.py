e9ebdf552c1f230e8fc12c678c47e20ff69e7eda
PonMorin <66424414+PonMorin@users.noreply.github.com>
2020-06-09 19:49:32 +0700

    Create Lecture50_Pon_M.py

---
+++ Lecture50_Pon_M.py
@@ -0,0 +1,16 @@
+def addnumber(x,y):
+    print(x+y)
+
+def Minus(x,y):
+    print(x-y)
+
+def Multiplied(x,y):
+    print(x*y)
+    
+def Divide(x,y):
+    print(x/y)
+
+addnumber(10,20)
+Minus(20,10)
+Multiplied(2,2)
+Divide(12,2)
